5ec1a5d83557ec911e7aeea91fbba461943acf38
ryan <rayanelghazzali@gmail.com>
2026-07-02 20:25:25 -0600

    Add PrivacyInfo.xcprivacy to App target

---
+++ ios/App/Pantre Food Widget/Pantre_Food_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Pantre_Food_WidgetBundle.swift
+//  Pantre Food Widget
+//
+//  Created by Ryan on 6/23/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Pantre_Food_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Pantre_Food_Widget()
+    }
+}
